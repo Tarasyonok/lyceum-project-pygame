e92ef4c703e3712588bc322acecf7a5c26b392ac
Kirill Tarasov <kir_tarasov@mail.ru>
2024-02-10 19:15:37 +0300

    feat: remove red attack rects

--- modules/attack.py
+++ modules/attack.py
@@ -7,33 +7,25 @@ class Attack(pygame.sprite.Sprite):
 
         if dir_x > 0 and dir_y == 0:
             self.image = pygame.Surface((35, player.rect.height))
-            self.image.fill((255, 0, 0))
             self.rect = self.image.get_rect(left=player.hitbox.center[0], top=player.rect.top)
         elif dir_x < 0 and dir_y == 0:
             self.image = pygame.Surface((35, player.rect.height))
-            self.image.fill((255, 0, 0))
             self.rect = self.image.get_rect(right=player.hitbox.center[0], top=player.rect.top)
         elif dir_x == 0 and dir_y < 0:
             self.image = pygame.Surface((28, player.rect.height))
-            self.image.fill((255, 0, 0))
             self.rect = self.image.get_rect(right=player.rect.right, bottom=player.hitbox.center[1])
         elif dir_x == 0 and dir_y > 0:
             self.image = pygame.Surface((28, player.rect.height))
-            self.image.fill((255, 0, 0))
             self.rect = self.image.get_rect(right=player.rect.right, top=player.hitbox.center[1])
         elif dir_x > 0 and dir_y > 0:
             self.image = pygame.Surface((player.rect.width, player.rect.width))
-            self.image.fill((255, 0, 0))
             self.rect = self.image.get_rect(left=player.hitbox.center[0], top=player.hitbox.center[1])
         elif dir_x > 0 and dir_y < 0:
             self.image = pygame.Surface((player.rect.width, player.rect.width))
-            self.image.fill((255, 0, 0))
             self.rect = self.image.get_rect(left=player.hitbox.center[0], bottom=player.hitbox.center[1])
         elif dir_x < 0 and dir_y < 0:
             self.image = pygame.Surface((player.rect.width, player.rect.width))
-            self.image.fill((255, 0, 0))
             self.rect = self.image.get_rect(right=player.hitbox.center[0], bottom=player.hitbox.center[1])
         elif dir_x < 0 and dir_y > 0:
             self.image = pygame.Surface((player.rect.width, player.rect.width))
-            self.image.fill((255, 0, 0))
             self.rect = self.image.get_rect(right=player.hitbox.center[0], top=player.hitbox.center[1])
