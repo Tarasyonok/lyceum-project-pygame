d30815bac3bae94000a78156fae89831d8e64ff1
Kirill Tarasov <kir_tarasov@mail.ru>
2024-02-24 22:18:27 +0300

    feat: make opening

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import pygame, sys
 from modules.settings import *
 from modules.level import Level
 from modules.start_menu import MainMenu
+from modules.opening import Opening
 from modules.debug import debug
 
 
@@ -33,11 +34,13 @@ class Game:
         # -------------------------------------
 
         self.clock = pygame.time.Clock()  # создаём инструмент тиков
-        exit_area = pygame.rect.Rect(500, 100, 300, 50)
-        self.level = Level('prod2', exit_area) # заггружаем уровень (Потом будем менять, т. к. уровней много. Здесь будет заставка)
+        # exit_area = pygame.rect.Rect(500, 100, 300, 50)
+        # 'prod2', exit_area
+        self.level = Level() # заггружаем уровень (Потом будем менять, т. к. уровней много. Здесь будет заставка)
         self.main_menu = MainMenu()
 
         self.start_game = False
+        self.start_opening = False
 
     def run(self):
         while True: # Главный цикл
@@ -47,20 +50,38 @@ class Game:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.MOUSEMOTION:
-                    self.main_menu.mouse_hover(event.pos)
+                    if not self.start_opening:
+                        self.main_menu.mouse_hover(event.pos)
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.main_menu.mouse_press(event.pos)
                 if event.type == pygame.MOUSEBUTTONUP:
                     if event.button == 1:
                         self.main_menu.mouse_click(event.pos)
-                        self.start_game = True
+                        self.opening = Opening()
+                        self.start_opening = True
+                        # self.start_game = True
+
+            # self.screen.fill((200, 200, 200)) # цвет фона
+
 
             if self.start_game:
                 self.screen.fill((10, 9, 9)) # цвет фона
                 self.level.run() # запускаем уровень
             else:
                 self.main_menu.show()
+
+            if self.start_opening:
+                self.opening.display()
+                if (not self.opening.start_opening_flag
+                        and not self.opening.tell_story_flag):
+                    self.start_game = True
+                if (not self.opening.start_opening_flag
+                        and not self.opening.tell_story_flag
+                        and not self.opening.end_opening_flag):
+                    self.start_opening = False
+
+
             pygame.display.update() # обновляем экран, а то просто чёрное всё будет
             self.clock.tick(FPS) # параметр 60 указывает выполнять цикл while 60 раз в секунду
 
--- modules/opening.py
+++ modules/opening.py
@@ -7,15 +7,21 @@ class Opening:
         self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
 
         self.story = [
-            "Hello",
-            "world",
+            ("Hello", 2000),
+            ("world", 2000),
         ]
 
-        half_width = self.display_surface.get_width()
+        self.text_color = pygame.color.Color((255, 255, 255))
+
+        width = self.display_surface.get_width()
         height = self.display_surface.get_height()
 
-        self.background = pygame.surface.Surface((half_width, height))
+        self.background = pygame.surface.Surface((width, height))
         self.background_rect = self.background.get_rect(topleft=(0, 0))
+        pygame.draw.rect(self.background, pygame.color.Color((0, 0, 0)), self.background_rect)
+
+        self.curr_text = self.font.render("", True, self.text_color)
+        self.curr_text_rect = self.curr_text.get_rect(center=(width * 0.5, height * 0.5))
 
         self.start_black = pygame.time.get_ticks()
         self.black_time = 1000
@@ -24,29 +30,75 @@ class Opening:
         self.tell_story_flag = False
         self.end_opening_flag = False
 
+        self.text_index = 0
+
+
 
     def start_opening(self):
         curr_time = pygame.time.get_ticks()
         if curr_time < self.start_black + self.black_time:
-            self.background.set_alpha(self.black_time / curr_time * 255)
+            self.background.set_alpha(255 * ((curr_time - self.start_black) / self.black_time))
         else:
             self.background.set_alpha(255)
             self.start_opening_flag = False
+            self.tell_story_flag = True
+            self.start_curr_text = pygame.time.get_ticks()
+            self.curr_text_delay = self.story[self.text_index][1]
+            self.pre_text_delay = 500
+            self.end_text_delay = 500
 
+            print()
 
-    def show_text(self, text, delta):
-        half_width = self.display_surface.get_width() // 2
-        half_height = self.display_surface.get_height() // 2
+            half_width = self.display_surface.get_width() // 2
+            half_height = self.display_surface.get_height() // 2
 
-        self.curr_text = self.font.render("SOMETHING", True, self.normal_color)
-        self.curr_text_rect = self.curr_text.get_rect(center=(half_width, half_height))
+            self.curr_text = self.font.render(self.story[self.text_index][0], True, self.text_color)
+            self.curr_text_rect = self.curr_text.get_rect(center=(half_width, half_height))
 
-    def end_opening(self):
-        pass
+            self.curr_text.set_alpha(0)
+
 
     def tell_story(self):
-        pass
+        curr_time = pygame.time.get_ticks()
+        if curr_time < self.start_curr_text + self.pre_text_delay:
+            self.curr_text.set_alpha(255 * ((curr_time - self.start_curr_text) / self.pre_text_delay))
+        else:
+            self.curr_text.set_alpha(255)
 
+        if 0 < curr_time - (self.start_curr_text + self.pre_text_delay + self.curr_text_delay) < self.end_text_delay:
+            self.curr_text.set_alpha(255 - 255 * ((curr_time - self.start_curr_text - self.pre_text_delay - self.curr_text_delay) / self.end_text_delay))
+        elif curr_time > self.start_curr_text + self.pre_text_delay + self.curr_text_delay + self.end_text_delay:
+            self.text_index += 1
+            if self.text_index >= len(self.story):
+                self.tell_story_flag = False
+                self.end_opening_flag = True
+                self.start_normal = pygame.time.get_ticks()
+                self.normal_time = 1000
+                return
+
+            half_width = self.display_surface.get_width() // 2
+            half_height = self.display_surface.get_height() // 2
+
+            self.start_curr_text = pygame.time.get_ticks()
+            self.curr_text_delay = self.story[self.text_index][1]
+
+            self.curr_text = self.font.render(self.story[self.text_index][0], True, self.text_color)
+            self.curr_text_rect = self.curr_text.get_rect(center=(half_width, half_height))
+
+            self.curr_text.set_alpha(0)
+
+
+
+
+
+    def end_opening(self):
+        curr_time = pygame.time.get_ticks()
+        if curr_time < self.start_normal + self.normal_time:
+            self.background.set_alpha(255 - 255 * ((curr_time - self.start_normal) / self.normal_time))
+        else:
+            self.background.set_alpha(0)
+            self.curr_text.set_alpha(0)
+            self.end_opening_flag = False
 
     def display(self):
         if self.start_opening_flag:
@@ -56,4 +108,5 @@ class Opening:
         elif self.end_opening_flag:
             self.end_opening()
         self.display_surface.blit(self.background, self.background_rect)
-        self.display_surface.blit(self.curr_text, self.curr_text_rect)
+        if self.tell_story_flag:
+            self.display_surface.blit(self.curr_text, self.curr_text_rect)
